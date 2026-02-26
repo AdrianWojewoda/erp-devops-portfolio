bb9e0c2f5a7b31a7455151e47ab39c7bfec6078d
Adrian Wojewoda <adrian.wojewoda@gmail.com>
2026-02-26 13:33:29 +0100

    feat(backend): add /metrics endpoint

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from app.db import check_db_connection
 
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
+
 app = FastAPI()
+
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
 
 @app.get("/readiness")
 def readiness():
@@ -14,3 +18,8 @@ def readiness():
         return {"database": "reachable"}
     except Exception as e:
         return {"database": "unreachable", "error": str(e)}
+
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
